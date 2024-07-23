cdfec8934dceb59de4367cc233178b14e50e0764
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-07-23 23:56:09 +0530

    second commit

--- prototypeinheritance/practice.js
+++ prototypeinheritance/practice.js
@@ -1,33 +1,61 @@
 // Factoryfunction
+// Animal.prototype.printType = function () {
+//     console.log(obj.type);
+// }
+
+
+// function Animal(type, legs) {
+//     let obj = {}
+//     Object.setPrototypeOf(obj, Animal.prototype)
+//     obj.type = type;
+//     obj.legs = legs;
+
+//     return obj;
+
+// }
+
+// let a1 = Animal("Domestic", 4)
+// console.log(a1);
+
+// Bird.prototype.printlegs = function () {
+//     console.log(obj.legs);
+// }
+// Object.setPrototypeOf(Bird.prototype, Animal.prototype)
+// function Bird(type, legs, canFly) {
+//     let obj = Animal(type, legs);
+//     Object.setPrototypeOf(obj, Bird.prototype)
+//     obj.canFly = canFly;
+
+//     return obj;
+// }
+
+// let b1 = Bird("wild", 2, false)
+// console.log(b1);
+
+
+
+// Constructorfunction
+
+
 Animal.prototype.printType = function () {
-    console.log(obj.type);
+    console.log(this.type);
 }
 
 
 function Animal(type, legs) {
-    let obj = {}
-    Object.setPrototypeOf(obj, Animal.prototype)
-    obj.type = type;
-    obj.legs = legs;
-
-    return obj;
-
+    this.type = type;
+    this.legs = legs;
 }
 
-let a1 = Animal("Dmestic", 4)
-console.log(a1);
+let animal = new Animal("Domestic", 4)
+console.log(animal);
 
-Bird.prototype.printlegs = function () {
-    console.log(obj.legs);
-}
+
 Object.setPrototypeOf(Bird.prototype, Animal.prototype)
-function Bird(type, legs, canFly) {
-    let obj = Animal(type, legs);
-    Object.setPrototypeOf(obj, Bird.prototype)
-    obj.canFly = canFly;
-
-    return obj;
+function Bird(type, legs, canfly) {
+    Animal.call(type, legs);
+    this.canfly = canfly
 }
 
-let b1 = Bird("wild", 2, false)
-console.log(b1);
+let bird = new Bird("domestic", 2, true)
+console.log(bird);
