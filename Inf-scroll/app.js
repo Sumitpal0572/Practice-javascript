ee0753d31b956573c045d189614206f25d9a1998
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-08-03 14:18:43 +0530

    inifity scrolling done

--- Inf-scroll/app.js
+++ Inf-scroll/app.js
@@ -1,20 +1,22 @@
-let url = `https://jsonplaceholder.typicode.com/photos?_page=1&_limit=10`
+// let api = `https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=10`
 let container = document.getElementById("container")
 
-async function fetchdata() {
-    res = await fetch(url)
+page = 1;
+
+async function fetchdata(api) {
+    res = await fetch(api)
     data = await res.json()
     console.log(data);
     displaydata(data)
 }
-fetchdata();
+fetchdata(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=10`);
 
 function displaydata(data) {
     data.forEach((ele) => {
         let card = document.createElement("div")
 
         let title = document.createElement("h3")
-        title.textContent = ele.Title
+        title.textContent = ele.title
 
         let image = document.createElement("img")
         image.textContent = ele.url
@@ -23,3 +25,19 @@ function displaydata(data) {
         container.append(card)
     })
 }
+
+window.addEventListener("scroll", function () {
+    let scrollheight = document.documentElement.scrollHeight;
+    let clientheight = document.documentElement.clientHeight;
+    let scrolltop = document.documentElement.scrollTop;
+
+    console.log(scrollheight, clientheight, scrolltop);
+
+
+    if (Math.ceil(scrollheight - clientheight) <= (Math.ceil(scrolltop))) {
+        console.log("reach the bottom");
+        page++
+        fetchdata(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=10`)
+
+    }
+})
